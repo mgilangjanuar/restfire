3fa7643e2ee1347012959172cf820770e7bb7a4a
M Gilang Januar <mgilangjanuar@gmail.com>
2020-10-23 20:51:14 +0700

    update electron.js

--- public/electron.js
+++ public/electron.js
@@ -4,13 +4,6 @@ const { autoUpdater } = require("electron-updater")
 require('./server')
 
 app.on('ready', () => {
-  autoUpdater.setFeedURL({
-    provider: 'github',
-    owner: 'mgilangjanuar',
-    repo: 'restfire',
-    token: '9589c5fa66de4dbe15663ef6aac16793380eb714'
-  })
-
   autoUpdater.checkForUpdatesAndNotify()
 
   const win = new BrowserWindow({
