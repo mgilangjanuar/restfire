4bcfcf8d47c61cbfde5da01c54e4aa06bd0f43ce
M Gilang Januar <mgilangjanuar@gmail.com>
2020-10-25 12:08:19 +0700

    fix flow updater

--- electron.js
+++ electron.js
@@ -10,22 +10,25 @@ autoUpdater.setFeedURL({
   provider: 'generic',
   url: 'https://lang-updater.herokuapp.com/download/latest'
 })
-autoUpdater.on('checking-for-update', () => console.log('Checking updates...'))
+autoUpdater.on('checking-for-update', () => win.webContents.executeJavaScript(`console.log('checking update...')`))
 autoUpdater.on('update-available', info => {
-  dialog.showMessageBox(null, {
-    type: 'question',
-    buttons: ['Download', 'Cancel'],
-    message: 'Download update?',
-    detail: `${info.version} released and will downloading in background.`
-  }, response => {
-    if (response === 0) {
-      autoUpdater.downloadUpdate()
-    }
-  })
+  win.webContents.executeJavaScript(`console.log('v${info.version} released and will downloading in background...')`)
+  // dialog.showMessageBox(null, {
+  //   type: 'question',
+  //   buttons: ['Download', 'Cancel'],
+  //   message: 'Download update?',
+  //   detail: `v${info.version} released and will downloading in background.`
+  // }, response => {
+  //   if (response === 0) {
+  //     autoUpdater.downloadUpdate()
+  //   }
+  // })
 })
-autoUpdater.on('error', error => console.error(`Error: ${error}`))
+autoUpdater.on('error', error => {
+  win.webContents.executeJavaScript(`console.error(${error})`)
+})
 autoUpdater.on('update-downloaded', () => {
-  console.log('Update downloaded!')
+  win.webContents.executeJavaScript(`console.log('Update downloaded!')`)
   dialog.showMessageBox(null, {
     type: 'question',
     buttons: ['Quit and install', 'Cancel'],
@@ -38,15 +41,17 @@ autoUpdater.on('update-downloaded', () => {
 })
 autoUpdater.on('download-progress', (info) => {
   const message = `Download speed: ${info.bytesPerSecond} - Downloaded ${info.percent}% (${info.transferred}/${info.total})`
-  console.log(message)
+  win.webContents.executeJavaScript(`console.log('${message}')`)
 })
 
 app.on('ready', () => {
-  autoUpdater.checkForUpdates()
   win = new BrowserWindow({
     icon: `${__dirname}/icon.png`
   })
   win.webContents.executeJavaScript(`if (window.localStorage.getItem('download-token')) window.localStorage.setItem('download-token', '${generate()}')`)
   win.maximize()
   win.loadURL('http://localhost:4002/app')
+  setTimeout(() => {
+    autoUpdater.checkForUpdatesAndNotify()
+  }, 2000)
 })
