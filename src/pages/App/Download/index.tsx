3d276d2ecf2c7c6b0ff848503868a2583b80a13a
M Gilang Januar <mgilangjanuar@gmail.com>
2020-10-25 14:22:57 +0700

    update download page

--- src/pages/App/Download/index.tsx
+++ src/pages/App/Download/index.tsx
@@ -1,19 +1,16 @@
-import { AppleOutlined, DownloadOutlined, QqOutlined, WindowsOutlined } from '@ant-design/icons'
+import { AppleFilled, DownloadOutlined, QqOutlined, WindowsFilled } from '@ant-design/icons'
 import { Button, Card, Typography } from 'antd'
 import React from 'react'
 
 const Download: React.FC = () => {
 
   return (
-    <Card title={<><DownloadOutlined /> Desktop App</>}>
-      <Typography.Paragraph>
-        Live Preview
-      </Typography.Paragraph>
+    <Card title={<><DownloadOutlined /> Desktop App (Live Preview)</>}>
       <Typography.Paragraph>
         <a href="https://lang-updater.herokuapp.com">
-          <Button style={{ marginBottom: '10px', marginRight: '10px' }} icon={<AppleOutlined />}>Mac OS</Button>
+          <Button style={{ marginBottom: '10px', marginRight: '10px' }} icon={<AppleFilled />}>Mac OS</Button>
         </a>
-        <Button style={{ marginBottom: '10px', marginRight: '10px' }} disabled icon={<WindowsOutlined />}>Windows (comming soon)</Button>
+        <Button style={{ marginBottom: '10px', marginRight: '10px' }} disabled icon={<WindowsFilled />}>Windows (comming soon)</Button>
         <Button style={{ marginBottom: '10px' }} disabled icon={<QqOutlined />}>Linux (comming soon)</Button>
       </Typography.Paragraph>
     </Card>
