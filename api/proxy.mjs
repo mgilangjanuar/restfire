ac583e16cf5880f49fae79115d956dc79225e249
M Gilang Januar <mgilangjanuar@gmail.com>
2023-03-20 06:37:47 +0700

    update serverless fn

--- api/proxy.mjs
+++ api/proxy.mjs
@@ -3,7 +3,24 @@ import FormData from 'form-data'
 import fs from 'fs'
 import os from 'os'
 
-export default async function handler(req, res) {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+async function handler(req, res) {
   const config = { ...req.body }
   if (config.headers && config.headers.contentType === 'multipart/form-data') {
     const formData = new FormData()
@@ -58,3 +75,5 @@ export default async function handler(req, res) {
     return res.send({ ...error.response ? { response } : {}, error })
   }
 }
+
+module.exports = allowCors(handler)
